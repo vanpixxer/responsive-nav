ec0328fb6b0f14b87393064a5593226c8a37a657
Darrell Chaddock <webtastic.dc@gmail.com>
2021-09-16 19:05:45 -0700

    Wrote simple JavaScript to make menu icon interactive

---
+++ app.js
@@ -0,0 +1,9 @@
+// classList - shows/gets all classes
+// toggle() - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function () {
+    links.classList.toggle("show-links");
+});
